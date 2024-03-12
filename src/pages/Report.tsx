9b62c35508cf1d727f35708c1888b69d119ca93d
hirose <yukihio76@gmail.com>
2024-03-12 23:52:43 +0900

    addComponent

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,8 +1,27 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
 
 const Report = () => {
+
+  const commonPaperStyle = {
+    height: {xs: 'auto', md: '400px'},
+    display: 'flex',
+    flexDirection: 'column'
+  }
+
   return (
-    <div>Report</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <MonthSelector />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}><CategoryChart /></Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}><BarChart /></Paper>
+      </Grid>
+      <Grid item xs={12}>テーブル</Grid>
+    </Grid>
   )
 }
 
